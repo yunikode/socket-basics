67e224961c7ca293d773c02ce55661d716e10dbf
zee-german <franz.lutzenberger@gmail.com>
2016-12-15 14:37:17 +0100

    socket.io on server and client added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # socket.io basics
 
+on [Heroku](https://urban-carnival.herokuapp.com/)
+
 [![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const path = require('path')
 
 const app = express()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(express.static(path.join(__dirname, '/public')))
+
+io.on('connection', () => {
+  console.log('user connected via socket.io')
+})
 
 http.listen(PORT, () => {
   console.log('sever started on port ' + PORT)
