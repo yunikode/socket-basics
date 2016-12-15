cf4cdd034fe645b4242fbe644969315bcecb6d07
zee-german <franz.lutzenberger@gmail.com>
2016-12-15 10:50:14 +0100

    basic express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3000
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const http = require('http').Server(app)
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+http.listen(PORT, () => {
+  console.log('sever started on port ' + PORT)
+})
